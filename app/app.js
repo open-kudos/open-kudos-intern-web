8b05d11e2ba8929b0b9c229f00d54d7c88197e3d
Cerkute <kristijonas.savanevicius@vka.lt>
2016-04-06 18:15:59 +0300

    Login form

--- app/app.js
+++ app/app.js
@@ -3,8 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'myApp.login',
+  'myApp.registration',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
