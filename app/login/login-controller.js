f615f093572d75c8138429411f4404dd96e5e610
Cerkute <kristijonas.savanevicius@vka.lt>
2016-04-12 16:54:12 +0300

    Code clean up in languages

--- app/login/login-controller.js
+++ app/login/login-controller.js
@@ -1,104 +1,91 @@
 'use strict';
 
 angular.module('myApp.login', ['ngRoute', 'ngCookies', 'base64'])
-    .config(['$routeProvider', function ($routeProvider) {
+    .config(function ($routeProvider) {
         $routeProvider.when('/login', {
             templateUrl: 'login/login.html',
             controller: 'loginController',
             controllerAs: 'login'
         });
-    }])
+    })
 
-    .controller('loginController', ['$scope', '$http', '$cookies', '$window', '$base64', 'SERVER', 'LoginService', function ($scope, $http, $cookies, $window, $base64, SERVER, LoginService) {
-        var language;
+    .controller('loginController', ['$scope', '$http', '$cookies', '$window', '$base64', 'SERVER', 'LoginService',
+        function ($scope, $http, $cookies, $window, $base64, SERVER, LoginService) {
+            initView();
 
-        initView();
+            /**
+             * Login function which takes values from form in login.html
+             * and makes POST call to the api via LoginService also
+             */
+            $scope.Login = function () {
 
-        if (!$cookies.get('language')) {
-            $cookies.put('language', 'en');
-            language = $cookies.get('language');
+                var rememberMe = $scope.rememberMeCheckbox;
 
-        } else language = $cookies.get('language');
-
-        if (language == 'en') {
-            $scope.title = "Login";
-            $scope.registration = 'Registration';
-            $scope.name = "Email";
-            $scope.password2 = 'Password';
-            $scope.forgotPassword = 'Forgot password';
-            $scope.loginButton = 'Login';
-            $scope.rememberMe = 'Remember me';
-        } else if (language == 'lt') {
-            $scope.title = "Prisijungimas";
-            $scope.registration = 'Registracija';
-            $scope.name = "El. paštas";
-            $scope.password2 = 'Slaptažodis';
-            $scope.forgotPassword = 'Pamiršai slaptažodį';
-            $scope.loginButton = 'Prisijungti';
-            $scope.rememberMe = 'Prisiminti';
-        }
-
-        $scope.lt = function () {
-            $scope.title = "Prisijungimas";
-            $scope.registration = 'Registracija';
-            $scope.name = "El. paštas";
-            $scope.password2 = 'Slaptažodis';
-            $scope.forgotPassword = 'Pamiršai slaptažodį';
-            $scope.loginButton = 'Prisijungti';
-            $scope.rememberMe = 'Prisiminti';
-            $cookies.put('language', 'lt');
-        };
-
-        $scope.en = function () {
-            $scope.title = "Login";
-            $scope.registration = 'Registration';
-            $scope.name = "Email";
-            $scope.password2 = 'Password';
-            $scope.forgotPassword = 'Forgot password';
-            $scope.loginButton = 'Login';
-            $scope.rememberMe = 'Remember me';
-            $cookies.put('language', 'en');
-        };
-
-        /**
-         * Login function which takes values from form in login.html
-         * and makes POST call to the api via LoginService also
-         */
-        $scope.Login = function () {
-
-            var rememberMe = $scope.rememberMeCheckbox;
-
-            var data = $.param({
-                email: this.email,
-                password: this.password
-            });
-
-            if (rememberMe) {
-                LoginService.rememberMe(data);
-            } else {
-                LoginService.login(data).then(function (val) {
-                    $scope.loginError = '';
-                }).catch(function () {
-                    loginValidation($scope.email, $scope.password);
+                var data = $.param({
+                    email: this.email,
+                    password: this.password
                 });
-            }
-        };
 
-        function initView() {
-            var ru = $cookies.get('ru');
-            if (ru === 'true') {
-                LoginService.login($base64.decode($cookies.get('e')));
+                if (rememberMe) {
+                    LoginService.rememberMe(data);
+                } else {
+                    LoginService.login(data).then(function (val) {
+                        $scope.loginError = '';
+                    }).catch(function () {
+                        loginValidation($scope.email, $scope.password);
+                    });
+                }
+            };
+
+            function initView() {
+                var ru = $cookies.get('ru');
+                if (ru === 'true') {
+                    LoginService.login($base64.decode($cookies.get('e')));
+                } else {
+                    LoginService.checkUser();
+                }
+            }
+
+            function loginValidation(email, password) {
+                if ((email == '') || (password == '')) {
+                    $scope.loginError = 'Wrong Email and Password';
+                } else {
+                    $scope.loginError = 'Wrong Email or Password';
+                }
+            }
+
+            /**
+             *  ******************************************************************
+             */
+            /**
+             ************************ Set language section ***********************
+             */
+            var lang = $cookies.get('language');
+
+            if (lang == null) {
+                $cookies.put('language', 'en');
+                setLanguage('en');
+                language = $cookies.get('language');
             } else {
-                LoginService.checkUser();
+                setLanguage(lang);
             }
-        }
 
-        function loginValidation (email, password){
-            if ((email == '') || (password == '')){
-                $scope.loginError = 'Enter Email and Password';
-            } else {
-                $scope.loginError = 'Wrong Email or Password';
+            $scope.lt = function () {
+                setLanguage('lt');
+                $cookies.put('language', 'lt');
+            };
+
+            $scope.en = function () {
+                setLanguage('en');
+                $cookies.put('language', 'en');
+            };
+
+            function setLanguage(language) {
+                $http.get('../app/translations/' + language + '.json').success(function (data) {
+                    $scope.language = data;
+                })
             }
-        }
-
+            /**
+             *  ********************* End of section *******************************
+             */
     }]);
