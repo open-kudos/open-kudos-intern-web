4d32c33e625392b298e5f33f4e3c00a6b8c80ede
Cerkute <kristijonas.savanevicius@vka.lt>
2016-06-21 15:56:53 +0300

    Closing issues for US 17 close #190 close #191 close #180 close #179

--- src/app/components/kudos-user-history/kudos-user-history.component.js
+++ src/app/components/kudos-user-history/kudos-user-history.component.js
@@ -1,18 +1,80 @@
 (function () {
-    var UserHistoryController = function(){
+    var UserHistoryController = function(Resources){
         var self = this;
 
+        self.associatedEmail = '';
+        self.historyRadioBox = 'wonLost';
+        self.receivedOperations = false;
         self.allOperations = true;
+        self.gaveOperations = false;
+        self.challengeOperations = false;
+        self.autocompleteHide = true;
+
         self.acornPlural = acornPlural;
+        self.changeRadioValue = changeRadioValue;
+        self.changeView = changeView;
+        self.selectAutoText = selectAutoText;
+        self.checkUserList = checkUserList;
 
         this.$onInit = function() {
             self.currentUser = this.user;
             self.modalIndex = split(this.user.$$hashKey, ":")[1];
         };
 
+        function changeRadioValue(value) {
+            if (self.historyRadioBox != value) {
+                self.historyRadioBox = value;
+                console.log(self.historyRadioBox);
+            }
+        }
+
+        function selectAutoText(text) {
+            self.associatedEmail = text;
+            self.searchTermSelected = false;
+            self.autocompleteHide = true;
+            self.text = text;
+        }
+        
+        function checkUserList() {
+            self.usersCollection = Resources.getUsersCollection();
+            if (self.searchTermSelected == false) {
+                if (self.associatedEmail != undefined) {
+                    if (self.text != self.associatedEmail)
+                        (self.associatedEmail.length > 1) ? self.autocompleteHide = false : self.autocompleteHide = true;
+                }
+            } else {
+                self.searchTermSelected = false;
+            }
+        }
+
+        function changeView(value) {
+            if (value == 'all') {
+                self.receivedOperations = false;
+                self.allOperations = true;
+                self.gaveOperations = false;
+                self.challengeOperations = false;
+            } else if (value == 'received'){
+                self.receivedOperations=true;
+                self.allOperations=false;
+                self.gaveOperations=false;
+                self.challengeOperations=false;
+            } else if (value == 'gave'){
+                self.receivedOperations=false;
+                self.allOperations=false;
+                self.gaveOperations=true;
+                self.challengeOperations=false;
+            } else if (value == 'challenges'){
+                self.receivedOperations=false;
+                self.allOperations=false;
+                self.gaveOperations=false;
+                self.challengeOperations=true;
+            }
+            self.associatedEmail = '';
+            self.historyRadioBox = 'wonLost';
+        }
     };
 
-    UserHistoryController.$inject = [];
+    UserHistoryController.$inject = ['Resources'];
 
     angular.module('myApp.components.userHistory', [])
         .component('kudosUserHistory', {
