81f19c2662f84172016105161884ce23d8057633
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-06-29 11:23:23 +0300

    More changes in acorns page controller

--- src/app/acorns/acorns.controller.js
+++ src/app/acorns/acorns.controller.js
@@ -1,12 +1,12 @@
 (function () {
+    'use strict';
     angular
         .module('myApp.acorn', [
             'ngRoute',
             'ngCookies'
         ])
         .controller('AcornController', AcornController);
-        'use strict';
-
+    
         AcornController.$inject = ['$scope', '$window', 'ProfileService', 'Resources'];
 
         function AcornController($scope, $window, ProfileService, Resources) {
@@ -29,18 +29,11 @@
             }
             activate();
 
-            $scope.$watch(function () {
-                return Resources.getUserAvailableKudos()
-            }, function (newVal) {
-                if (!isValid(newVal)) $scope.userAvailableKudos = Resources.getUserAvailableKudos();
-            });
-
             function checkUser() {
                 ProfileService.checkUser().then(function (val) {
                     val.logged ? $window.location.href = "#/acorns" : $window.location.href = "#/login";
                 });
             }
         }
-
 
     })();
