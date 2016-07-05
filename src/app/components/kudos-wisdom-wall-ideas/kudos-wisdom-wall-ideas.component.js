89428ae890b8e1086bb1b8e7d5ddd8aaad664a6d
Cerkute <kristijonas.savanevicius@vka.lt>
2016-07-05 15:26:02 +0300

    Wisdom wall refactored

--- src/app/components/kudos-wisdom-wall-ideas/kudos-wisdom-wall-ideas.component.js
+++ src/app/components/kudos-wisdom-wall-ideas/kudos-wisdom-wall-ideas.component.js
@@ -1,20 +1,22 @@
 (function () {
-    var WisdomWallIdeasController = function ($scope, WisdomWallIdeasService){
-        $scope.showLoader = true;
 
-        WisdomWallIdeasService.getRandomIdea().then(function(val) {
-            $scope.randomQuote = val;
-            $scope.showLoader = false;
-        });
-
-    };
-
-    WisdomWallIdeasController.$inject = ['$scope', 'WisdomWallIdeasService'];
+    WisdomWallIdeasController.$inject = ['WisdomWallIdeasService'];
 
     angular.module('myApp.components.wisdomWallIdeas', [])
         .component('wisdomWallIdeas', {
             templateUrl: 'app/components/kudos-wisdom-wall-ideas/kudos-wisdom-wall-ideas.html',
-            controller: 'WisdomWallIdeasController'
-        })
-        .controller('WisdomWallIdeasController',  WisdomWallIdeasController)
+            controller: ('WisdomWallIdeasController',  WisdomWallIdeasController),
+            controllerAs: 'wisdom'
+        });
+
+    function WisdomWallIdeasController(WisdomWallIdeasService){
+        var vm = this;
+
+        vm.showLoader = true;
+
+        WisdomWallIdeasService.getRandomIdea().then(function(val) {
+            vm.randomQuote = val;
+            vm.showLoader = false;
+        });
+    }
 })();
