56f3337fce42bf60a114bb194325b8e6d13a3c65
Jurgis Bridzius <jurgis@bridzius.lt>
2016-07-08 23:08:33 +0300

    Travis should work - pretty please?

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 module.exports = function (config) {
-    config.set({
-
+    var configuration = {
         basePath: 'src',
 
         files: [
@@ -52,7 +51,20 @@ module.exports = function (config) {
         junitReporter: {
             outputFile: 'test_out/unit.xml',
             suite: 'unit'
+        },
+
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
         }
 
-    });
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };
