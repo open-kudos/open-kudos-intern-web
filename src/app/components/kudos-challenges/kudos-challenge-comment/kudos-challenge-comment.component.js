6718b244e4882894cb388e78cd4a23c6810b084c
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-08-01 16:40:22 +0300

    Added minimal comment input validation and pagination for comments

--- src/app/components/kudos-challenges/kudos-challenge-comment/kudos-challenge-comment.component.js
+++ src/app/components/kudos-challenges/kudos-challenge-comment/kudos-challenge-comment.component.js
@@ -14,42 +14,83 @@
 
     function ChallengeCommentController(Challenges) {
         var vm = this;
-        var requestParams = {page: 0, size: 5};
+        
+        var pageParams = {page: 0, size: 5};
+        var pageResponse;
 
         vm.message = "";
         vm.commentsCollection = [];
 
         vm.addComment = addComment;
         vm.formatDate = formatDate;
+        vm.loadNextPage = loadNextPage;
+        vm.loadPreviousPage = loadPreviousPage;
 
         vm.$onInit = onInit();
 
         function onInit() {
-            console.log(vm.challenge);
-            getComments();
+            getComments(pageParams);
         }
 
         function addComment() {
+            vm.loading = true;
             var comment = {comment : vm.message};
 
-            Challenges.addComment(vm.challenge.id, comment).then(function (response) {
-                if (response.status === 200){
-                    vm.message = null;
-                    getComments();
-                }
+            if ( vm.message.length > 0 ) {
+                Challenges.addComment(vm.challenge.id, comment).then(function (response) {
+                    if (response.status === 200) {
+                        vm.message = "";
+                        getComments(pageParams);
+                        vm.loading = false;
+                    }
+                })
+            } else {
+                vm.loading = false;
+                toastr.error("Your comment is empty");
+            }
+
+        }
+
+        function getComments(pageParams) {
+            vm.loading = true;
+            Challenges.getComments(vm.challenge.id, pageParams).then(function (response) {
+                pageResponse = response;
+                vm.totalElements = response.totalElements;
+                vm.commentsCollection = response.content;
+                checkPaginationButtons(response);
+                vm.loading = false;
             })
         }
 
-        function getComments() {
-            Challenges.getComments(vm.challenge.id, requestParams).then(function (pageResponse) {
-                console.log(pageResponse);
-                vm.commentsCollection = pageResponse.content;
-            })
+        function loadPreviousPage() {
+            if (!pageResponse.first) pageParams.page--;
+            getComments(pageParams);
+        }
+
+        function loadNextPage() {
+            if (!pageResponse.last) pageParams.page++;
+            getComments(pageParams);
+        }
+
+        /*function loadChallengesHistory(pageParams) {
+            vm.loading = true;
+            Challenges.getChallengesHistory(pageParams).then(function (response) {
+                ChallengesPanelService.setCompletedChallengesAmount(response.totalElements);
+                pageResponse = response;
+                vm.completedChallengesCollection = response.content;
+                Resources.setCompletedChallenges(response.content);
+                checkPaginationButtons(response);
+                vm.loading = false;
+            });
+        }*/
+
+        function checkPaginationButtons(pageResponse) {
+            vm.showMoreButton = !pageResponse.last;
+            vm.showLessButton = !pageResponse.first;
         }
 
         function formatDate(commentDate) {
             var date = new Date(commentDate);
-            console.log(date);
             return date.getFullYear() + "-" + formatDateNumber(date.getMonth()) + "-" + formatDateNumber(date.getDay())+ "-" + date.getHours() + ":" + date.getMinutes();
         }
 
