079fe5c1737ebd8c8ca0df3200d251f3f7215448
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-08-11 11:33:48 +0300

    close #251

--- src/app/components/kudos-relationship-feed/kudos-feed-challenge/feed-challenge.component.js
+++ src/app/components/kudos-relationship-feed/kudos-feed-challenge/feed-challenge.component.js
@@ -23,7 +23,54 @@
         vm.$onInit = onInit();
 
         function onInit() {
+            checkActionType(vm.action.type);
+        }
 
+        function checkActionType(type) {
+            switch (type) {
+                case 'CREATED_CHALLENGE' :
+                    vm.for = true;
+                    vm.fromForText = "for";
+                    vm.challengeTypeText = "created a new";
+                    break;
+                case 'CANCELED_CHALLENGE' :
+                    vm.for = true;
+                    vm.fromForText = "for";
+                    vm.challengeTypeText = "canceled";
+                    break;
+                case 'DECLINED_CHALLENGE' :
+                    vm.from = true;
+                    vm.fromForText = "from";
+                    vm.challengeTypeText = "declined";
+                    break;
+                case 'ACCEPTED_CHALLENGE' :
+                    vm.from = true;
+                    vm.fromForText = "from";
+                    vm.challengeTypeText = "accepted";
+                    break;
+                case 'FAILED_CHALLENGE' :
+                    vm.from = true;
+                    vm.fromForText = "from";
+                    vm.challengeTypeText = "failed";
+                    break;
+                case 'MARKED_AS_COMPLETED' :
+                    vm.for = true;
+                    vm.fromForText = "for";
+                    vm.challengeTypeText = "marked as completed";
+                    break;
+                case 'MARKED_AS_FAILED' :
+                    vm.for = true;
+                    vm.fromForText = "for";
+                    vm.challengeTypeText = "marked as failed";
+                    break;
+                case 'ACCOMPLISHED_CHALLENGE' :
+                    vm.from = true;
+                    vm.challengeTypeText = "accomplished";
+                    vm.fromForText = "from";
+                    break;
+                default:
+                    vm.hidePanel = true;
+            }
         }
 
     }
